9b05a13d0aa06a68f74c27c25d9de7899ba61d86
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-03-21 03:56:41 -0700

    Update UpgradeManager.php

--- src/terpz710/orefactory/generator/UpgradeManager.php
+++ src/terpz710/orefactory/generator/UpgradeManager.php
@@ -6,16 +6,15 @@ namespace terpz710\orefactory\generator;
 
 use pocketmine\player\Player;
 
-use terpz710\orefactory\OreFactory;
+use pocketmine\utils\Config;
 
-use poggit\libasynql\DataConnector;
-use poggit\libasynql\libasynql;
+use terpz710\orefactory\OreFactory;
 
 final class UpgradeManager {
 
-    protected DataConnector $db;
+    private Config $data;
 
-    protected array $generatorSpeeds = [
+    private array $generatorSpeeds = [
         1 => 10,
         2 => 9,
         3 => 8,
@@ -23,35 +22,25 @@ final class UpgradeManager {
         5 => 6
     ];
 
-    public function __construct(protected OreFactory $plugin) {
+    public function __construct(private OreFactory $plugin) {
         $this->plugin = $plugin;
-    }
-
-    public function table() : void{
-        $this->db = libasynql::create($this->plugin, $this->plugin->getConfig()->get("database"), [
-            "sqlite" => "database/sqlite.sql",
-            "mysql" => "database/mysql.sql"
-        ]);
-
-        $this->db->executeGeneric("table.generators");
+        
+        $this->data = new Config($this->plugin->getDataFolder() . "data.json");
     }
 
     public function init(Player $player) : void {
         $uuid = $player->getUniqueId()->toString();
 
-        $this->db->executeSelect("generators.select_level", ["uuid" => $uuid], function(array $rows) use ($uuid) {
-            if (empty($rows)) {
-                $this->db->executeChange("generators.insert", ["uuid" => $uuid]);
-            }
-        });
+        if (!$this->data->exists($uuid)) {
+            $this->data->set($uuid, ["level" => 1]);
+            $this->data->save();
+        }
     }
 
     public function getGeneratorLevel(Player $player, callable $callback) : void{
         $uuid = $player->getUniqueId()->toString();
-
-        $this->db->executeSelect("generators.select_level", ["uuid" => $uuid], function(array $rows) use ($callback) {
-            $callback($rows[0]["level"]);
-        });
+        $level = $this->data->get($uuid, ["level" => 1])["level"];
+        $callback($level);
     }
 
     public function upgradeGenerator(Player $player) : void{
@@ -64,7 +53,8 @@ final class UpgradeManager {
             }
 
             $newLevel = $currentLevel + 1;
-            $this->db->executeChange("generators.update_level", ["uuid" => $uuid, "level" => $newLevel]);
+            $this->data->set($uuid, ["level" => $newLevel]);
+            $this->data->save();
 
             $player->sendMessage("§aGenerator upgraded to level " . $newLevel . "!");
         });
@@ -74,9 +64,5 @@ final class UpgradeManager {
         $this->getGeneratorLevel($player, function(int $level) use ($callback) {
             $callback($this->generatorSpeeds[$level]);
         });
-    }
-
-    public function close() : void{
-        $this->db->close();
     }
 }
